3b9f0813c67d9aadda0e83483001985260c2b8fb
Euan Wallace <s1936140@ed/ac.uk>
2023-01-15 13:52:04 +0000

    first commit

---
+++ vulnerable-web-app/listoflogins.sh
@@ -0,0 +1,4 @@
+python3 -m webbrowser http://localhost/setup.php/
+python3 -m webbrowser http://localhost/vulnerabilities/brute/?username=aaa&password=bbb
+python3 -m webbrowser http://localhost/vulnerabilities/brute/?username=aa&password=bb&Login=Login#
+python3 -m webbrowser http://localhost/vulnerabilities/brute/?username=admin&password=password&Login=Login#
